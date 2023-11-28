c566956ca098e63b70ded9ba1be87d5a3e7271dd
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2023-11-27 17:57:43 -0800

    Update server IP

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 import socket
 import random
 
-HOST = "localhost"  # Standard loopback interface address (localhost)
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 8888  # Port to listen on (non-privileged ports are > 1023)
 password = b"test"
 
