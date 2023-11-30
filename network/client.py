692605dc26a586d00edf3ffe0e139c4bba4b4c34
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2023-11-29 23:22:53 -0800

    Turn server and class into different objects

---
+++ network/client.py
@@ -0,0 +1,80 @@
+import socket
+from threading import Thread
+import pygame as pg
+
+
+class Client:
+    def __init__(self):
+        self._HOST: str = ""
+        self._PORT: int = 0
+        self.conn: socket.socket = None
+        self.running: bool = False
+
+    @property
+    def HOST(self):
+        return self._HOST
+
+    @HOST.setter
+    def HOST(self, HOST):
+        if not self.HOST:
+            self._HOST = HOST
+        else:
+            raise AttributeError(f"Attribute already assigned to value")
+
+    @property
+    def PORT(self):
+        return self._PORT
+
+    @PORT.setter
+    def PORT(self, PORT):
+        if not self.PORT:
+            self._PORT = PORT
+        else:
+            raise AttributeError(f"Attribute already assigned to value")
+    
+    def decode_client_key(self, text) -> None:
+        """
+        Initialize client host and port
+        """
+        try:
+            sprtr = text[0]
+            tokens = text[1:len(text)-1].split(sprtr)
+            self.PORT = int(tokens[0] + tokens[len(tokens)-1])
+            tokens = tokens[1:len(tokens)-1]
+            decoder = tokens[(len(tokens)-1)//2+1:]
+            host_tokens = tokens[:(len(tokens)-1)//2+1]
+            self.HOST = ".".join(host_tokens[int(index)] for index in decoder)
+        except ValueError:
+            print("Invalid key!")
+    
+    def start_client(self) -> None:
+        """
+        Connect to server
+        """
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect((self.HOST, self.PORT))
+        self.running = True
+        send = Thread(target=self.run_client)
+        send.start()
+    
+    def run_client(self) -> None:
+        """
+        Communicating with server
+        """
+        try:
+            data = self.conn.recv(1024)
+            if data == b"1":
+                raise ConnectionRefusedError
+            print(f"Successfully Connected! Data outputted: {data.decode()}")
+        except ConnectionAbortedError:
+            pass
+    
+    def key_decoded(self) -> bool:
+        """
+        Check if HOST and PORT attributes already have
+        values.
+        """
+        return self._HOST and self._PORT
+    
+    def close_client(self) -> None:
+        self.conn.close()
---
+++ network/server.py
@@ -0,0 +1,126 @@
+import socket
+from threading import Thread
+from random import randint, sample
+
+HOST = socket.gethostbyname(socket.gethostname()) 
+PORT = randint(0, 65535)
+password = b"test"
+
+
+class Server:
+    def __init__(self, password: str = None,
+                 host: str = socket.gethostbyname(socket.gethostname()),
+                 port: int = randint(0, 65535), max_connections: int = 200):
+        self.__HOST: str = host
+        self.__PORT: int = port
+        self.__KEY = None
+        self.__PASSWORD = password
+        self.running: bool = False
+        self.__SERVER: socket.socket = None
+        self.max_conn = max_connections
+        self.clients_conn = 0
+
+    @property
+    def HOST(self):
+        return self.__HOST
+
+    @HOST.setter
+    def HOST(self, HOST: str):
+        if not self.HOST:
+            self.__HOST: str = HOST
+        else:
+            raise AttributeError(f"Attribute already assigned to value")
+
+    @property
+    def PORT(self):
+        return self.__PORT
+
+    @PORT.setter
+    def PORT(self, PORT: int):
+        if not self.PORT:
+            self.__PORT: int = PORT
+        else:
+            raise AttributeError(f"Attribute already assigned to value")
+    
+    @property
+    def KEY(self):
+        return self.__KEY
+    
+    @KEY.setter
+    def KEY(self, KEY: str):
+        if not self.KEY:
+            self.__KEY: int = KEY
+        else:
+            raise AttributeError(f"Attribute already assigned to value")
+    
+    @property
+    def PASSWORD(self):
+        return self.__PASSWORD
+    
+    @property
+    def SERVER(self):
+        return self.__SERVER
+
+    @SERVER.setter
+    def SERVER(self, SERVER: socket.socket):
+        if not self.SERVER:
+            self.__SERVER: socket.socket = SERVER
+        else:
+            raise AttributeError(f"Attribute already assigned to value")
+
+    def create_client_key(self) -> None:
+        """
+        Create key for client to connect to server
+        """
+        sprtr = chr(randint(35, 38))
+        temp = self.HOST.split(".")
+        sep_host = sample(temp, k=len(temp))
+        mess_ip = sprtr.join(part for part in sep_host)
+        decoder = sprtr.join(str(sep_host.index(part)) for part in temp)
+        del temp, sep_host
+        self.KEY = f"{sprtr}{str(self.PORT)[:len(str(self.PORT))//2]}{sprtr}{mess_ip}{sprtr}{decoder}{sprtr}{str(self.PORT)[len(str(self.PORT))//2:]}{sprtr}"
+    
+    def start_server(self) -> None:
+        """
+        Start the server main loop
+        """
+        self.SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.SERVER.bind((self.HOST, self.PORT))
+        self.running = True
+        if not self.KEY:
+            self.create_client_key()
+        print(f"Started server...\nKEY: {self.KEY}")
+        listen = Thread(target=self.run_server())
+        listen.start()
+    
+    def run_server(self) -> None:
+        """
+        Continuously check for connections
+        """
+        while self.clients_conn < self.max_conn:
+            self.SERVER.listen()
+            conn, addr = self.SERVER.accept()
+            process = Thread(target=self.accept_client, args=(conn, addr))
+            process.start()
+    
+    def accept_client(self, conn, addr) -> None:
+        """
+        Handle different clients
+        """
+        try:
+            with conn:
+                self.clients_conn += 1
+                print(f"Current # of clients connected: {self.clients_conn}")
+                print(f"Connected by {addr}")
+                data = conn.recv(1024)
+                if data.decode() == password.decode():
+                    conn.sendall(b"Connection Allowed")
+                else:
+                    conn.sendall(b"1")
+        except ConnectionResetError:
+            print(f"Connection closed with {addr}. Updated # of clients connected: {self.clients_conn-1}")
+        self.clients_conn -= 1
+        print(f"Current # of clients connected: {self.clients_conn}")
+    
+    def close_server(self):
+        self.SERVER.close()
