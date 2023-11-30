a1d1be2edf4ad6da4d987daf71718ba04975b010
EBarringer <142259171+EBarringer@users.noreply.github.com>
2023-11-30 13:17:17 -0800

    Update sudoku_creator.py

--- puzzle creators/sudoku_creator.py
+++ puzzle creators/sudoku_creator.py
@@ -1,19 +1,12 @@
-# function that intakes a nested array and returns a new array that first reverses the order
-# of elements, and then capitalizes every other letter
-# ['a', 'b', 'c', ['d', 'e', 'f'], 'g'] => ['C', 'b', 'A', ['f', 'E', 'd'], 'G']
-
-def reverse_nested(arr : list, counter:int=0) -> list:
-    narr = []
-    for index in range(len(arr)):
-        if isinstance(arr[index], list):
-            temp = arr[index]
-            temp.insert(0, 'a')
-            narr.append(reverse_nested(arr[index]))[1:]
-        else:
-            if not (index+1) % 2:
-                narr.append(arr[index].upper())
-            else:
-                narr.append(arr[index])
-    return narr
-
-print(reverse_nested(['a', 'b', 'c', ['d', 'e', 'f']]))
+# script to initialize the sudoko board with a random setup
+from random import randint as r
+####### BACKGROUND #######
+# the board is a 9x9 grid made up of 9 3x3 smaller square grids
+class Board:
+    def __init__(self):
+        self.sGrid1 = [0, 0, 0]
+        self.sGrid2 = [0, 0, 0]
+        self.sGrid3 = [0, 0, 0]
+        self.sGrid4 = [0, 0, 0]
+        self.sGrid5 = [0, 0, 0]
+        self.sGrid
