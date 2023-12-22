37524a0b3952c18750035d8843cc7d07a3c7fa6a
Elliott Barringer <elliottbarringer@icloud.org>
2023-12-22 15:47:56 -0800

    fixing things --> still errors

--- gen_data.py
+++ gen_data.py
@@ -1,1 +1,7 @@
 from models.sudoku.generate_raw import Generator
+generator = Generator(
+    iterations=100,
+    X_train_path='./xtrain.txt',
+    y_train_path='./ytrain.txt'
+    )
+generator.generate()
--- models/sudoku/generate_raw.py
+++ models/sudoku/generate_raw.py
@@ -3,33 +3,46 @@ from random import randint as r
 class Generator:
     def __init__(self, iterations : int, X_train_path : str, y_train_path : str):
         self.iterations = iterations 
-        self.base_grid : list[list[int]] = [
-            [0, 0, 0, 6, 9, 0, 0, 1, 7],
-            [8, 0, 1, 3, 0, 0, 4, 0, 0],
-            [9, 3, 0, 1, 0, 4, 0, 8, 0],
-            [0, 0, 2, 0, 0, 0, 0, 0, 0],
-            [5, 0, 9, 8, 7, 2, 6, 0, 1],
-            [0, 0, 0, 0, 0, 0, 7, 0, 0],
-            [0, 7, 0, 5, 0, 9, 0, 4, 3],
-            [0, 0, 8, 0, 0, 3, 1, 0, 9],
-            [3, 9, 0, 0, 6, 1, 0, 0, 0]
-        ]
+        '''self.base_grid : list[list[int]] = [
+		    [7, 0, 0, 0, 0, 0, 0, 0, 0],
+		    [0, 0, 0, 8, 0, 0, 1, 0, 0],
+		    [0, 2, 0, 0, 0, 0, 0, 0, 9],
+		    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+		    [0, 0, 0, 0, 0, 6, 0, 0, 0],
+		    [0, 3, 0, 0, 8, 0, 0, 6, 0],
+		    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+		    [0, 0, 4, 0, 0, 0, 0, 0, 0],
+		    [0, 0, 0, 0, 0, 0, 0, 5, 0]
+	    ]'''
+        self.base_grid : list[list[int]] = [[0 for _ in range(9)] for _ in range(9)]
         self.X_train_path = X_train_path
         self.y_train_path = y_train_path
     def generate(self):
-        for iteration in self.iterations:
+        for iteration in range(1, self.iterations):
             print(f'Iteration {iteration} : Generating Sudoku Board')
+            self.printgap()
             board : [list[list[int]]] = [[0 for _ in range(9)] for _ in range(9)]
-            for idx in range(81):
-                i, j = divmod(idx, 9)
-                board[i][j] = self.base_grid[i][j] 
-            board = self.solve(board)
+            print('EMPTY BOARD')
+            print(str(board))
+            self.printgap()
+            for i in range(9):
+                for j in range(9):
+                    board[i][j] = self.base_grid[i][j]
+            print('COPIED BOARD') 
+            print(str(board))
+            self.printgap()
+            for i in range(9):
+                for j in range(9):
+                    board = self.solve(board, i, j)
+            print('SOLVED BOARD')
+            print(board)
+            self.printgap()
             for _ in range(r(5, 10)):
                 row, col = r(0, 8), r(0, 8)
                 board[row][col] = 0
             self.update_X(board)
-    def solve(self, board : list[list[int]]) -> list[list[int]]:
-        empty_cell = self.find_empty(board) 
+    def solve(self, board : list[list[int]], i : int, j : int) -> list[list[int]]:
+        '''empty_cell = self.find_empty(board) 
         if not empty_cell:
             self.update_y(board)
             return board 
@@ -41,9 +54,26 @@ class Generator:
                     self.update_y(board)
                     return board 
                 board[row][col] = 0 
-        ValueError(f'inputted board {str(board)} has no solution')
-        return None
-    def find_empty(self, board : list[list][int]) -> tuple(int, int):
+        raise ValueError(f'inputted board {str(board)} has no solution')'''
+        while board[i][j] != 0:
+            if i < 8:
+                i += 1
+            elif i == 8 and j < 8:
+                i = 0
+                j += 1
+            elif i == 8 and j == 8:
+                return board
+            for it in range(1, 10):
+                if self.valid(board, i, j, it):
+                    board[i][j] = it 
+                    if self.solve(board, i, j) == 1:
+                        return board 
+                    else:
+                        board[i][j] = 0
+        print('NO SOLUTION')
+        self.printgap() 
+        return board
+    def find_empty(self, board : list[list[int]]):
         for idx in range(81):
             i, j = divmod(idx, 9)
             if board[i][j] == 0:
@@ -53,10 +83,10 @@ class Generator:
         if num in board[row] or num in [board[i][col] for i in range(9)]:
             return False 
         start_row, start_col = 3 * (row // 3), 3 * (col // 3)
-        for idx in range(9):
-            i, j = divmod(idx, 3)
-            if board[start_row + i][start_col + j] == num:
-                return False 
+        for i in range(3):
+            for j in range(3):
+                if board[start_row + i][start_col + j] == num:
+                    return False 
         return True
     def update_y(self, board : list[list[int]]) -> None:
         with open(self.y_train_path, 'a+') as f:
@@ -76,3 +106,7 @@ class Generator:
                 string += str(it)
             f.write(string)
             f.write('\n')
+    def printgap(self):
+        print('\n')
+        print('-----------------------------------------------------------------------')
+        print('\n')
