9f5c5d6b0274daa8b3fffb0dc4d60622f8316315
Elliott Barringer <elliottbarringer@icloud.org>
2023-12-22 00:55:24 -0800

    starting something special

---
+++ models/sudoku/generate_raw.py
@@ -0,0 +1,16 @@
+class Generator:
+    def __init__(self, iterations : int):
+        self.iterations = iterations 
+        self.base_grid : list[list[int]] = [
+            [0, 0, 0, 6, 9, 0, 0, 1, 7],
+            [8, 0, 1, 3, 0, 0, 4, 0, 0],
+            [9, 3, 0, 1, 0, 4, 0, 8, 0],
+            [0, 0, 2, 0, 0, 0, 0, 0, 0],
+            [5, 0, 9, 8, 7, 2, 6, 0, 1],
+            [0, 0, 0, 0, 0, 0, 7, 0, 0],
+            [0, 7, 0, 5, 0, 9, 0, 4, 3]
+        ]
+    def generate(self):
+        for iteration in self.iterations:
+            print(f'Iteration {iteration} : Generating Sudoku Board')
+            
