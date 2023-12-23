f6bb4fcc9e956e519e467b7a075d2bee64233ab5
Elliott Barringer <elliottbarringer@icloud.org>
2023-12-23 02:00:00 -0800

    finished writing train data

--- gen_data.py
+++ gen_data.py
@@ -1,7 +1,13 @@
 from models.sudoku.generate_raw import Generator
-generator = Generator(
-    iterations=100,
-    X_train_path='./xtrain.txt',
-    y_train_path='./ytrain.txt'
-    )
-generator.generate()
+
+for i in range(0, 100):
+    for j in range(0, 4):
+        g = Generator(
+            iterations=51,
+            X_train_path='./xtrain.txt',
+            y_train_path='./ytrain.txt',
+            base_grid_select=j
+        )
+        print(str(g.base_grid))
+        print(f'GENERATOR {j}')
+        g.generate()
--- models/sudoku/generate_raw.py
+++ models/sudoku/generate_raw.py
@@ -1,24 +1,114 @@
 from random import randint as r
 
 class Generator:
-    def __init__(self, iterations : int, X_train_path : str, y_train_path : str):
+    def __init__(self, iterations : int, X_train_path : str, y_train_path : str, base_grid_select : int):
+        '''
+        base_grid_select starts at 0, indexing the sudoku puzzles 
+        '''
         self.iterations = iterations 
-        '''self.base_grid : list[list[int]] = [
-		    [7, 0, 0, 0, 0, 0, 0, 0, 0],
-		    [0, 0, 0, 8, 0, 0, 1, 0, 0],
-		    [0, 2, 0, 0, 0, 0, 0, 0, 9],
-		    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-		    [0, 0, 0, 0, 0, 6, 0, 0, 0],
-		    [0, 3, 0, 0, 8, 0, 0, 6, 0],
-		    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-		    [0, 0, 4, 0, 0, 0, 0, 0, 0],
-		    [0, 0, 0, 0, 0, 0, 0, 5, 0]
-	    ]'''
-        self.base_grid : list[list[int]] = [[0 for _ in range(9)] for _ in range(9)]
+        '''sudoku_puzzles : list[list[list[int]]] = [
+            [
+                [0, 0, 0, 2, 0, 0, 0, 6, 0],
+                [0, 0, 9, 0, 0, 1, 0, 0, 8],
+                [6, 0, 0, 0, 0, 0, 4, 0, 0],
+                [0, 8, 0, 0, 0, 0, 0, 2, 0],
+                [4, 0, 0, 8, 0, 5, 0, 0, 6],
+                [0, 2, 0, 0, 0, 0, 0, 7, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 7],
+                [9, 0, 0, 4, 0, 0, 6, 0, 0],
+                [2, 0, 0, 0, 0, 8, 0, 5, 4]
+            ],
+            [
+                [0, 0, 0, 0, 0, 3, 0, 0, 8],
+                [0, 0, 0, 9, 0, 0, 0, 7, 0],
+                [0, 0, 7, 0, 0, 0, 0, 4, 0],
+                [1, 0, 0, 4, 0, 0, 0, 0, 0],
+                [4, 0, 0, 0, 0, 0, 0, 9, 0],
+                [0, 0, 2, 0, 0, 9, 0, 0, 0],
+                [0, 0, 0, 0, 0, 8, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 5, 0, 0],
+                [0, 0, 3, 0, 0, 0, 0, 0, 6]
+            ],
+            [
+                [0, 9, 0, 7, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 6, 0],
+                [0, 0, 0, 8, 0, 0, 0, 0, 0],
+                [0, 0, 0, 9, 0, 0, 0, 7, 2],
+                [7, 0, 0, 0, 0, 0, 0, 0, 4],
+                [0, 2, 0, 0, 0, 0, 0, 0, 8],
+                [0, 0, 0, 0, 0, 2, 0, 0, 0],
+                [0, 5, 0, 0, 0, 0, 9, 0, 0],
+                [0, 0, 6, 0, 0, 3, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0, 0, 4, 3, 0, 0],
+                [0, 6, 3, 0, 0, 0, 9, 2, 0],
+                [0, 0, 0, 0, 9, 0, 0, 1, 8],
+                [0, 0, 0, 6, 1, 0, 0, 0, 7],
+                [0, 8, 0, 0, 0, 0, 0, 3, 0],
+                [7, 0, 0, 0, 5, 3, 0, 0, 0],
+                [2, 4, 0, 0, 8, 0, 0, 0, 0],
+                [0, 7, 1, 0, 0, 0, 4, 6, 0],
+                [0, 0, 8, 1, 0, 0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 5, 0, 0, 0, 0, 0],
+                [0, 0, 0, 9, 3, 0, 0, 2, 0],
+                [0, 0, 0, 0, 0, 0, 8, 0, 7],
+                [0, 0, 5, 3, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 7, 0, 0],
+                [0, 2, 1, 0, 0, 0, 0, 0, 9],
+                [4, 0, 7, 0, 0, 6, 0, 0, 0],
+                [0, 8, 0, 0, 7, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 2, 1, 0]
+            ],
+        ]
+        self.base_grid = sudoku_puzzles[base_grid_select]'''
+        grid1 = [
+                [7, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 8, 0, 0, 1, 0, 0],
+                [0, 2, 0, 0, 0, 0, 0, 0, 9],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 6, 0, 0, 0],
+                [0, 3, 0, 0, 8, 0, 0, 6, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 4, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 5, 0]
+            ]
+        grid2 = [
+                [0, 0, 0, 0, 0, 3, 0, 9, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [6, 0, 8, 2, 0, 0, 0, 7, 0],
+                [0, 0, 0, 0, 5, 0, 0, 0, 0],
+                [0, 0, 3, 0, 0, 0, 8, 0, 0],
+                [0, 0, 0, 0, 7, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 7, 0, 9],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 2, 0, 8, 0, 0, 0, 0, 0]
+            ]
+        if base_grid_select == 0:
+            self.base_grid : list[list[int]] = grid1
+        elif base_grid_select == 1:
+            self.base_grid : list[list[int]] = grid2
+        elif base_grid_select == 2:
+            reversed = grid1[::-1]
+            full_reversed = [inner_list[::-1] for inner_list in reversed]
+            self.base_grid : list[list[int]] = full_reversed
+        elif base_grid_select == 3:
+            reversed = grid2[::-1]
+            full_reversed = [inner_list[::-1] for inner_list in reversed]
+            self.base_grid : list[list[int]] = full_reversed
+        else:
+            self.base_grid : list[list[int]] = grid1
+        #self.base_grid : list[list[int]] = [[0 for _ in range(9)] for _ in range(9)]
         self.X_train_path = X_train_path
         self.y_train_path = y_train_path
+        self.x_updated : bool = False 
+        self.y_updated : bool = False
     def generate(self):
         for iteration in range(1, self.iterations):
+            unsolved_board : list[list[int]] = []
+            solved_board : list[list[int]] = []
             print(f'Iteration {iteration} : Generating Sudoku Board')
             self.printgap()
             board : [list[list[int]]] = [[0 for _ in range(9)] for _ in range(9)]
@@ -33,15 +123,18 @@ class Generator:
             self.printgap()
             for i in range(9):
                 for j in range(9):
-                    board = self.solve(board, i, j)
+                    self.solve(board, i, j)
+            solved_board = board
+            self.update_y(solved_board)
             print('SOLVED BOARD')
             print(board)
             self.printgap()
-            for _ in range(r(5, 10)):
+            for _ in range(r(10, 15)):
                 row, col = r(0, 8), r(0, 8)
                 board[row][col] = 0
-            self.update_X(board)
-    def solve(self, board : list[list[int]], i : int, j : int) -> list[list[int]]:
+            unsolved_board = board
+            self.update_X(unsolved_board)
+    def solve(self, board : list[list[int]], i : int, j : int) -> bool:
         '''empty_cell = self.find_empty(board) 
         if not empty_cell:
             self.update_y(board)
@@ -62,17 +155,15 @@ class Generator:
                 i = 0
                 j += 1
             elif i == 8 and j == 8:
-                return board
+                return True
             for it in range(1, 10):
                 if self.valid(board, i, j, it):
                     board[i][j] = it 
-                    if self.solve(board, i, j) == 1:
-                        return board 
+                    if self.solve(board, i, j):
+                        return True
                     else:
                         board[i][j] = 0
-        print('NO SOLUTION')
-        self.printgap() 
-        return board
+        return False
     def find_empty(self, board : list[list[int]]):
         for idx in range(81):
             i, j = divmod(idx, 9)
@@ -98,6 +189,11 @@ class Generator:
             f.write(string)
             f.write('\n')
     def update_X(self, board : list[list[int]]) -> None:
+        i, j = self.find_empty(board)
+        if board[i][j] == 0:
+            self.printgap()
+            print('SOLUTION CONTAINS ZEROES')
+            self.printgap()
         with open(self.X_train_path, 'a+') as f:
             string : str = ''
             for idx in range(81):
